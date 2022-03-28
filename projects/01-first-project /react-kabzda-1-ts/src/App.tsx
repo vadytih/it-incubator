d779831df26cb153e7a90c3e3b9a9a0acccd4896
Vadim <vady.tih@gmail.com>
2022-03-28 14:23:59 +0300

    ДЗ видео (01 - React кабзда как подробно (копоненты)) - создать компанент 'Star'

--- projects/01-first-project /react-kabzda-1-ts/src/App.tsx
+++ projects/01-first-project /react-kabzda-1-ts/src/App.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Technologies from "./Technologies";
+import Star from "./Star";
+
+
+// function hello () {
+//     alert('hello')
+// }
+// hello();
 
 const App = () => {
-  return (
-      <div>
-        <Header/>
-        <Technologies/>
-      </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <div>
+                <Star/>
+                <Star/>
+                <Star/>
+                <Star/>
+                <Star/>
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- projects/01-first-project /react-kabzda-1-ts/src/Header.tsx
+++ projects/01-first-project /react-kabzda-1-ts/src/Header.tsx
@@ -3,9 +3,18 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            <a href="#">Home</a>
-            <a href="#">News Feed</a>
-            <a href="#">Messages</a>
+            <ul>
+                <h3>Меню</h3>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">News Feed</a>
+                </li>
+                <li>
+                    <a href="#">Messages</a>
+                </li>
+            </ul>
         </div>
     )
 }
