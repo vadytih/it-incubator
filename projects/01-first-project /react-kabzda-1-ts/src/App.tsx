d66ed0b4c307296269429ca0494387ab947f2876
Vadim <vady.tih@gmail.com>
2022-03-28 12:53:09 +0300

    react-kabzda-1-ts готово

---
+++ projects/01-first-project /react-kabzda-1-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+const App = () => {
+  return (
+      <div>
+        <Header/>
+        <Technologies/>
+      </div>
+  )
+}
+
+export default App;
