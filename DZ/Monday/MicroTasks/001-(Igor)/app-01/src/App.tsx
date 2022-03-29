4787344ee0ec9b1e397fa91658973e15c9d8d136
Vadim <vady.tih@gmail.com>
2022-03-29 21:37:43 +0300

    Микротаск map() - решено! понято

--- DZ/Monday/MicroTasks/001-(Igor)/app-01/src/App.tsx
+++ DZ/Monday/MicroTasks/001-(Igor)/app-01/src/App.tsx
@@ -4,13 +4,34 @@ import {Header} from "./components/Header";
 import {Body} from "./components/Body";
 import {Footer} from "./components/Footer";
 
+const students = [
+    {id: 1, name: "James", age: 8},
+    {id: 2, name: "Robert", age: 18},
+    {id: 3, name: "John", age: 28},
+    {id: 4, name: "Michael", age: 38},
+    {id: 5, name: "William", age: 48},
+    {id: 6, name: "David", age: 58},
+    {id: 7, name: "Richard", age: 68},
+    {id: 8, name: "Joseph", age: 78},
+    {id: 9, name: "Thomas", age: 88},
+    {id: 10, name: "Charles", age: 98},
+    {id: 10, name: "Christopher", age: 100},
+]
+
+const topCars = [
+    {manufacturer:'BMW', model:'m5cs'},
+    {manufacturer:'Mercedes', model:'e63s'},
+    {manufacturer:'Audi', model:'rs6'}
+]
+
 function App() {
   return (
     <div className="App">
         <>
             <Header contentHeader={"HEADER"}/>
-            <Body contentBody={"BODY content"}/>
+            <Body studentsList={students} studentsTitle={"Студенты"} autoList={topCars} autoListTitle={"Авто"}/>
             <Footer contentFooter={"FOOTER content"}/>
+
         </>
     </div>
   );
--- DZ/Monday/MicroTasks/001-(Igor)/app-01/src/components/Body.tsx
+++ DZ/Monday/MicroTasks/001-(Igor)/app-01/src/components/Body.tsx
@@ -1,13 +1,48 @@
 import React from "react";
 
 type BodyPropsType ={
-    contentBody: string
+    studentsTitle: string
+    autoListTitle: string
+    studentsList: Array<SudentObj>
+    autoList: Array<AutoObj>
+
+}
+
+type SudentObj = {
+    id: number
+    name: string
+    age: number
+
+}
+
+type AutoObj = {
+    manufacturer: string
+    model: string
 }
 
 export const Body = (props: BodyPropsType) => {
     return (
         <>
-            <div>{props.contentBody}</div>
+            <h2>{props.studentsTitle}</h2>
+            {
+                props.studentsList.map((obj,index) => {
+                    return (
+                        <ul>
+                            <li key={index}>{obj.id} {obj.name} {obj.age}</li>
+                        </ul>
+                    )
+                })
+            }
+            <h2>{props.autoListTitle}</h2>
+            {
+                props.autoList.map((obj, index)=>{
+                    return(
+                        <ul>
+                            <li key={index}>{obj.manufacturer} {obj.model}</li>
+                        </ul>
+                    )
+                })
+            }
         </>
     )
 }
