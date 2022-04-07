b24247d36bd45898d0f42769ee1dcfe1b34ca2ca
Vadim <vady.tih@gmail.com>
2022-04-07 10:52:47 +0300

    задачи от Игната 1 ДЗ

---
+++ homeworks-by-ignat/homeworks/src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageType) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
