0e9d8b9bd21045eb9d083ef06f7fad8b8f10ff88
Vadim <vady.tih@gmail.com>
2022-04-21 14:45:05 +0300

    Задача от Игната №3

--- homeworks-by-ignat/homeworks/src/p2-homeworks/h3/Greeting.tsx
+++ homeworks-by-ignat/homeworks/src/p2-homeworks/h3/Greeting.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (event: React.ChangeEvent<HTMLInputElement>)=>void // need to fix any
+    addUser: ()=>void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error ? s.error : '' // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onChange={(event)=>setNameCallback(event)} className={inputClass}/>
             <span>{error}</span>
             <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <span> {totalUsers}</span>
         </div>
     )
 }
